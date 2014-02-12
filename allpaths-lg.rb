69eee4d1724112f4327a4b03f15bceafe05c7700
Shaun Jackman <sjackman@gmail.com>
2014-02-11 22:24:54 -0800

    allpaths-lg 48686
    Fails to build with clang and gcc-4.2.
    error: You must compile this with g++ 4.7 or higher.

--- allpaths-lg.rb
+++ allpaths-lg.rb
@@ -2,8 +2,18 @@ require 'formula'
 
 class AllpathsLg < Formula
   homepage 'http://www.broadinstitute.org/software/allpaths-lg/blog/'
-  url 'ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-47032.tar.gz'
-  sha1 'c53cfe3443d769ddd2a77b61e2c600b3cb49bb2a'
+  url 'ftp://ftp.broadinstitute.org/pub/crd/ALLPATHS/Release-LG/latest_source_code/allpathslg-48686.tar.gz'
+  sha1 '21780e413eed7041ef7c6bfcd16ec6258d0264b9'
+
+  fails_with :clang do
+    build 500
+    cause 'You must compile this with g++ 4.7 or higher.'
+  end
+
+  fails_with :gcc do
+    build 5666
+    cause 'You must compile this with g++ 4.7 or higher.'
+  end
 
   def install
     system './configure', '--disable-debug', '--disable-dependency-tracking',
